9c08fb4444b8b6582344bae3d2573e3a9815ba3e
AndreaJDS <andrea_jolene@outlook.com>
2023-02-24 14:36:24 -0500

    Solved Maven app to display greeting based on time

--- src/main/java/maven/maven/App.java
+++ src/main/java/maven/maven/App.java
@@ -1,5 +1,7 @@
 package maven.maven;
 
+import java.time.LocalTime;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to COMP367" );
+    	LocalTime currentTime = LocalTime.now();
+    	String greeting = "";
+    	if (currentTime.isBefore(LocalTime.NOON)) {
+    	    greeting = "Good morning";
+    	} else {
+    	    greeting = "Good afternoon";
+    	}
+    	String studentName = "Andrea"; // Replace this with the actual student's name
+    	String welcomeMessage = String.format("%s, %s, Welcome to COMP367!", greeting, studentName);
+    	System.out.println(welcomeMessage);
+
     }
 }
